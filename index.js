0a84e90bb56e36005100198c435a87fefa35c56f
Fabbri Paolo <jabawack81@gmail.com>
2019-06-24 23:26:08 +0100

    remove heroku files

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000
-app
-  .use(express.static(`${__dirname}/dist`))
-  .get('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
-  .listen(port, () => console.log(`Express has started on port: ${port}`));
