750ce8f8a7a0da4af37bd3c297b9825c8158d556
Fabbri Paolo <jabawack81@gmail.com>
2019-07-17 00:16:22 +0100

    add favicon and touch icon

--- gulpfile.js
+++ gulpfile.js
@@ -1,120 +1,209 @@
 // generated on 2019-03-01 using generator-webapp 4.0.0-2
-const { src, dest, watch, series, parallel, lastRun } = require('gulp');
-const gulpLoadPlugins = require('gulp-load-plugins');
-const browserSync = require('browser-sync');
-const del = require('del');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-const { argv } = require('yargs');
+const { src, dest, watch, series, parallel, lastRun } = require("gulp");
+const gulpLoadPlugins = require("gulp-load-plugins");
+const browserSync = require("browser-sync");
+const del = require("del");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const { argv } = require("yargs");
+var realFavicon = require("gulp-real-favicon");
+var fs = require("fs");
+
+// File where the favicon markups are stored
+var FAVICON_DATA_FILE = "faviconData.json";
 
 const $ = gulpLoadPlugins();
 const server = browserSync.create();
 
 const port = argv.port || 9000;
 
-const isProd = process.env.NODE_ENV === 'production';
-const isTest = process.env.NODE_ENV === 'test';
+const isProd = process.env.NODE_ENV === "production";
+const isTest = process.env.NODE_ENV === "test";
 const isDev = !isProd && !isTest;
 
+// Generate the icons. This task takes a few seconds to complete.
+// You should run it at least once to create the icons. Then,
+// you should run it whenever RealFaviconGenerator updates its
+// package (see the check-for-favicon-update task below).
+function generateFavicon(done) {
+  realFavicon.generateFavicon(
+    {
+      masterPicture: "app/favicon.png",
+      dest: "dist/",
+      iconsPath: "/",
+      design: {
+        ios: {
+          pictureAspect: "noChange",
+          assets: {
+            ios6AndPriorIcons: false,
+            ios7AndLaterIcons: false,
+            precomposedIcons: false,
+            declareOnlyDefaultIcon: true,
+          },
+        },
+        desktopBrowser: {},
+        windows: {
+          pictureAspect: "noChange",
+          backgroundColor: "#2b5797",
+          onConflict: "override",
+          assets: {
+            windows80Ie10Tile: false,
+            windows10Ie11EdgeTiles: {
+              small: false,
+              medium: true,
+              big: false,
+              rectangle: false,
+            },
+          },
+        },
+        androidChrome: {
+          pictureAspect: "noChange",
+          themeColor: "#ffffff",
+          manifest: {
+            startUrl: "https://ic.internetblacksmith.co.uk/",
+            display: "standalone",
+            orientation: "notSet",
+            onConflict: "override",
+            declared: true,
+          },
+          assets: {
+            legacyIcon: false,
+            lowResolutionIcons: false,
+          },
+        },
+        safariPinnedTab: {
+          pictureAspect: "blackAndWhite",
+          threshold: 20,
+          themeColor: "#5bbad5",
+        },
+      },
+      settings: {
+        scalingAlgorithm: "Mitchell",
+        errorOnImageTooSmall: false,
+        readmeFile: false,
+        htmlCodeFile: false,
+        usePathAsIs: false,
+      },
+      markupFile: FAVICON_DATA_FILE,
+    },
+    function() {
+      done();
+    },
+  );
+}
+
 function styles() {
-  return src('app/styles/*.scss')
+  return src("app/styles/*.scss")
     .pipe($.plumber())
     .pipe($.if(!isProd, $.sourcemaps.init()))
-    .pipe($.sass.sync({
-      outputStyle: 'expanded',
-      precision: 10,
-      includePaths: ['.']
-    }).on('error', $.sass.logError))
-    .pipe($.postcss([
-      autoprefixer()
-    ]))
+    .pipe(
+      $.sass
+        .sync({
+          outputStyle: "expanded",
+          precision: 10,
+          includePaths: ["."],
+        })
+        .on("error", $.sass.logError),
+    )
+    .pipe($.postcss([autoprefixer()]))
     .pipe($.if(!isProd, $.sourcemaps.write()))
-    .pipe(dest('.tmp/styles'))
-    .pipe(server.reload({stream: true}));
-};
+    .pipe(dest(".tmp/styles"))
+    .pipe(server.reload({ stream: true }));
+}
 
 function scripts() {
-  return src('app/scripts/**/*.js')
+  return src("app/scripts/**/*.js")
     .pipe($.plumber())
     .pipe($.if(!isProd, $.sourcemaps.init()))
     .pipe($.babel())
-    .pipe($.if(!isProd, $.sourcemaps.write('.')))
-    .pipe(dest('.tmp/scripts'))
-    .pipe(server.reload({stream: true}));
-};
+    .pipe($.if(!isProd, $.sourcemaps.write(".")))
+    .pipe(dest(".tmp/scripts"))
+    .pipe(server.reload({ stream: true }));
+}
 
 const lintBase = files => {
   return src(files)
     .pipe($.eslint({ fix: true }))
-    .pipe(server.reload({stream: true, once: true}))
+    .pipe(server.reload({ stream: true, once: true }))
     .pipe($.eslint.format())
     .pipe($.if(!server.active, $.eslint.failAfterError()));
-}
+};
 function lint() {
-  return lintBase('app/scripts/**/*.js')
-    .pipe(dest('app/scripts'));
-};
+  return lintBase("app/scripts/**/*.js").pipe(dest("app/scripts"));
+}
 function lintTest() {
-  return lintBase('test/spec/**/*.js')
-    .pipe(dest('test/spec'));
-};
+  return lintBase("test/spec/**/*.js").pipe(dest("test/spec"));
+}
 
 function html() {
-  return src('app/*.html')
-    .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
-    .pipe($.if(/\.js$/, $.uglify({compress: {drop_console: true}})))
-    .pipe($.if(/\.css$/, $.postcss([cssnano({safe: true, autoprefixer: false})])))
-    .pipe($.if(/\.html$/, $.htmlmin({
-      collapseWhitespace: true,
-      minifyCSS: true,
-      minifyJS: {compress: {drop_console: true}},
-      processConditionalComments: true,
-      removeComments: true,
-      removeEmptyAttributes: true,
-      removeScriptTypeAttributes: true,
-      removeStyleLinkTypeAttributes: true
-    })))
-    .pipe(dest('dist'));
+  return src("app/index.html")
+    .pipe($.useref({ searchPath: [".tmp", "app", "."] }))
+    .pipe($.if(/\.js$/, $.uglify({ compress: { drop_console: true } })))
+    .pipe(
+      $.if(/\.css$/, $.postcss([cssnano({ safe: true, autoprefixer: false })])),
+    )
+    .pipe(
+      $.if(
+        /\.html$/,
+        realFavicon.injectFaviconMarkups(
+          JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code,
+        ),
+      ),
+    )
+    .pipe(
+      $.if(
+        /\.html$/,
+        $.htmlmin({
+          collapseWhitespace: true,
+          minifyCSS: true,
+          minifyJS: { compress: { drop_console: true } },
+          processConditionalComments: true,
+          removeComments: true,
+          removeEmptyAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+        }),
+      ),
+    )
+    .pipe(dest("dist"));
 }
 
 function images() {
-  return src('app/images/**/*', { since: lastRun(images) })
+  return src("app/images/**/*", { since: lastRun(images) })
     .pipe($.imagemin())
-    .pipe(dest('dist/images'));
-};
+    .pipe(dest("dist/images"));
+}
 
 function fonts() {
-  return src('app/fonts/**/*.{eot,svg,ttf,woff,woff2}')
-    .pipe($.if(!isProd, dest('.tmp/fonts'), dest('dist/fonts')));
-};
+  return src("app/fonts/**/*.{eot,svg,ttf,woff,woff2}").pipe(
+    $.if(!isProd, dest(".tmp/fonts"), dest("dist/fonts")),
+  );
+}
 
 function extras() {
-  return src([
-    'app/*',
-    '!app/*.html'
-  ], {
-    dot: true
-  }).pipe(dest('dist'));
-};
+  return src(["app/*", "!app/*.html"], {
+    dot: true,
+  }).pipe(dest("dist"));
+}
 
 function clean() {
-  return del(['.tmp', 'dist'])
+  return del([".tmp", "dist"]);
 }
 
 function measureSize() {
-  return src('dist/**/*')
-    .pipe($.size({title: 'build', gzip: true}));
+  return src("dist/**/*").pipe($.size({ title: "build", gzip: true }));
 }
 
 const build = series(
   parallel(
     lint,
     series(parallel(styles, scripts), html),
+    generateFavicon,
     images,
     fonts,
-    extras
+    extras,
   ),
-  measureSize
+  measureSize,
 );
 
 function startAppServer() {
@@ -122,22 +211,21 @@ function startAppServer() {
     notify: false,
     port,
     server: {
-      baseDir: ['.tmp', 'app'],
+      baseDir: [".tmp", "app"],
       routes: {
-        '/node_modules': 'node_modules'
-      }
-    }
+        "/node_modules": "node_modules",
+      },
+    },
   });
 
-  watch([
-    'app/*.html',
-    'app/images/**/*',
-    '.tmp/fonts/**/*'
-  ]).on('change', server.reload);
+  watch(["app/*.html", "app/images/**/*", ".tmp/fonts/**/*"]).on(
+    "change",
+    server.reload,
+  );
 
-  watch('app/styles/**/*.scss', styles);
-  watch('app/scripts/**/*.js', scripts);
-  watch('app/fonts/**/*', fonts);
+  watch("app/styles/**/*.scss", styles);
+  watch("app/scripts/**/*.js", scripts);
+  watch("app/fonts/**/*", fonts);
 }
 
 function startTestServer() {
@@ -146,17 +234,17 @@ function startTestServer() {
     port,
     ui: false,
     server: {
-      baseDir: 'test',
+      baseDir: "test",
       routes: {
-        '/scripts': '.tmp/scripts',
-        '/node_modules': 'node_modules'
-      }
-    }
+        "/scripts": ".tmp/scripts",
+        "/node_modules": "node_modules",
+      },
+    },
   });
 
-  watch('app/scripts/**/*.js', scripts);
-  watch(['test/spec/**/*.js', 'test/index.html']).on('change', server.reload);
-  watch('test/spec/**/*.js', lintTest);
+  watch("app/scripts/**/*.js", scripts);
+  watch(["test/spec/**/*.js", "test/index.html"]).on("change", server.reload);
+  watch("test/spec/**/*.js", lintTest);
 }
 
 function startDistServer() {
@@ -164,11 +252,11 @@ function startDistServer() {
     notify: false,
     port,
     server: {
-      baseDir: 'dist',
+      baseDir: "dist",
       routes: {
-        '/node_modules': 'node_modules'
-      }
-    }
+        "/node_modules": "node_modules",
+      },
+    },
   });
 }
 
@@ -185,3 +273,4 @@ exports.lint = lint;
 exports.serve = serve;
 exports.build = build;
 exports.default = build;
+exports.generateFavicon = generateFavicon;
